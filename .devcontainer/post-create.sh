197c749d9d43979219c3dbd168f5da41d305f6fd
Kaelem <kc@kaelem.net>
2025-12-25 03:37:01 +0000

    Remove small/big mode conditional

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,14 +6,9 @@ sudo sysctl -w fs.inotify.max_user_instances=512
 
 cd /workspaces/playground
 
-if [ "$1" = "SMALL" ]; then
-    echo "small"
-    docker load -i /opt/k3s.tar
-    k3d cluster create eda-demo --k3s-arg "--disable=traefik@server:*" --k3s-arg "--disable=servicelb@server:*"
-    make try-eda NO_KIND=yes NO_LB=yes
-else
-    make try-eda
-fi
+docker load -i /opt/k3s.tar
+k3d cluster create eda-demo --k3s-arg "--disable=traefik@server:*" --k3s-arg "--disable=servicelb@server:*"
+make try-eda NO_KIND=yes NO_LB=yes
 
 echo "export PATH=$PATH:/workspaces/playground/tools" >> ~/.zshrc
 make configure-codespaces-keycloak
