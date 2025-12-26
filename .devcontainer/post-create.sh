01d123bc01ebc8fde3f5f58eee1fc614ef9dd7f0
Kaelem Chandra <kc@kaelem.net>
2025-12-27 12:19:23 +1300

    Use zstd compressed image

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,10 +6,15 @@ sudo sysctl -w fs.inotify.max_user_instances=512
 
 cd /workspaces/playground
 
+date
+docker pull ghcr.io/kaelemc/k3s-eda:latest-zstd
+date
+
+
 k3d cluster create eda-demo \
     --k3s-arg "--disable=traefik@server:*" \
     --k3s-arg "--disable=servicelb@server:*" \
-    --image ghcr.io/kaelemc/k3s-eda:latest
+    --image ghcr.io/kaelemc/k3s-eda:latest-zstd
 
 date
 make try-eda NO_KIND=yes NO_LB=yes
