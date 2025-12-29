bf0822dfb73b10a2d781fb7a766e77980a3e21b0
Kaelem Chandra <kc@kaelem.net>
2025-12-30 00:17:28 +1300

    Prebuild 4

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -e
 
-sudo mkdir -p /etc/docker /workspaces/.docker-data
-echo '{"data-root": "/workspaces/.docker-data"}' | sudo tee /etc/docker/daemon.json
-
-sudo pkill dockerd || true
-sleep 1
-sudo dockerd > /dev/null 2>&1 &
-
 echo "Waiting for Docker daemon..."
 until docker info > /dev/null 2>&1; do
     sleep 1
@@ -16,5 +9,3 @@ echo "Docker daemon is ready"
 
 docker pull ghcr.io/kaelemc/k3s-eda:latest-zstd
 docker tag ghcr.io/kaelemc/k3s-eda:latest-zstd k3s-eda:latest
-# sudo zstd -d -c /root/eda.tar.zst | docker load
-# sudo rm /root/eda.tar.zst
--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,15 +4,6 @@ set -e
 sudo sysctl -w fs.inotify.max_user_watches=1048576
 sudo sysctl -w fs.inotify.max_user_instances=512
 
-date
-
-sudo mkdir -p /etc/docker
-echo '{"data-root": "/workspaces/.docker-data"}' | sudo tee /etc/docker/daemon.json
-
-sudo pkill dockerd || true
-sleep 1
-sudo dockerd > /dev/null 2>&1 &
-
 echo "Waiting for Docker daemon..."
 until docker info > /dev/null 2>&1; do
     sleep 1
@@ -25,8 +16,7 @@ k3d cluster create eda-demo \
     --k3s-arg "--disable=traefik@server:*" \
     --k3s-arg "--disable=servicelb@server:*" \
     --image k3s-eda:latest \
-    --no-lb \
-    --no-tools
+    --no-lb
 
 date
 make try-eda NO_KIND=yes NO_LB=yes
