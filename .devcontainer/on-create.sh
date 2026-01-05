3e85caa8c39c5fa8934ea5d6c8427fb076f7901f
Kaelem Chandra <kc@kaelem.net>
2026-01-06 02:36:51 +1300

    Adjust PATH

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -4,4 +4,4 @@ TESTTOKEN=$(echo -n "bm9raWEtZWRhLWJvdDpnaHBfZHJOUXhvbWVrYUJrZUg2MXQ5aEk5U2QzTVN
 until crictl info >/dev/null 2>&1; do sleep 1; done
 setsid bash -c "cat /opt/images.txt | xargs -P 0 -I {} crictl pull --creds $TESTTOKEN {}" >/dev/null 2>&1 &
 make download-tools
-echo "export PATH=$PATH:/workspaces/k3s-codespaces-test/playground/tools" > /etc/profile.d/eda-tools.sh
+echo "export PATH=$PATH:/workspaces/playground/tools" > /etc/profile.d/eda-tools.sh
