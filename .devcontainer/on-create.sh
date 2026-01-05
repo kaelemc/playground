74481863c36222cc39c3e6a9232dc730b9b2febb
Kaelem Chandra <kc@kaelem.net>
2026-01-06 02:21:58 +1300

    background pulls + pure k3s vol

---
+++ .devcontainer/on-create.sh
@@ -0,0 +1,6 @@
+ln -sf /host-docker.sock /var/run/docker.sock
+TESTTOKEN=$(echo -n "bm9raWEtZWRhLWJvdDpnaHBfZHJOUXhvbWVrYUJrZUg2MXQ5aEk5U2QzTVNoMzF1MUVRMVJ4" | base64 -d)
+until crictl info >/dev/null 2>&1; do sleep 1; done
+setsid bash -c "cat /opt/images.txt | xargs -P 0 -I {} crictl pull --creds $TESTTOKEN {}" >/dev/null 2>&1 &
+make download-tools
+echo "export PATH=$PATH:/workspaces/k3s-codespaces-test/playground/tools" > /etc/profile.d/eda-tools.sh
