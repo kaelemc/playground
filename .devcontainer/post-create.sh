955681b1e4cfd35b6473d00a456ee125998dba17
Kaelem <kc@kaelem.net>
2026-01-05 20:58:26 +0000

    Wait for Docker again in the post-create

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,6 +3,12 @@ set -e
 
 cd /workspaces/playground
 
+echo "Waiting for Docker daemon..."
+until docker info > /dev/null 2>&1; do
+    sleep 1
+done
+echo "Docker daemon is ready"
+
 date
 make try-eda NO_KIND=yes NO_LB=yes
 date
