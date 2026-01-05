a42bc084917a6268bb31c33c5b95ae6113a21de5
Kaelem Chandra <kc@kaelem.net>
2026-01-06 02:32:57 +1300

    Enable shared mount for root overlayfs

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -1,3 +1,4 @@
+mount --make-rshared /
 ln -sf /host-docker.sock /var/run/docker.sock
 TESTTOKEN=$(echo -n "bm9raWEtZWRhLWJvdDpnaHBfZHJOUXhvbWVrYUJrZUg2MXQ5aEk5U2QzTVNoMzF1MUVRMVJ4" | base64 -d)
 until crictl info >/dev/null 2>&1; do sleep 1; done
